8e8363f210153826f4aa3f9f817a60160ecbe27a
Ilya Prokaznikov <ramzone255@mail.ru>
2024-08-01 03:15:36 +0300

    Persistence for Club

---
+++ RunGroopWebApp/RunGroopWebApp/Interfaces/IClubRepository.cs
@@ -0,0 +1,15 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Interfaces
+{
+    public interface IClubRepository
+    {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsyns(int id);
+        Task<IEnumerable<Club>> GetClubBySity(string sity);
+        bool Add(Club club);
+        bool Update(Club club);
+        bool Delete(Club club);
+        bool Save();
+    }
+}
---
+++ RunGroopWebApp/RunGroopWebApp/Repository/ClubRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ClubRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public bool Add(Club club)
+        {
+            _context.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Remove(club);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsyns(int id)
+        {
+            return await _context.Clubs.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Club>> GetClubBySity(string sity)
+        {
+            return await _context.Clubs.Where(p => p.Address.City.Contains(sity)).ToArrayAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Club club)
+        {
+            _context.Update(club);
+            return Save();
+        }
+    }
+}
